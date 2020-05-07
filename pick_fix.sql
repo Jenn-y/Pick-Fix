3520fb3a7c37392b29a4deef592bc48ebedec3bb
[Dzenita Djulovic] <[djulovic.dzenita1999@gmail.com]>
2020-05-07 15:19:14 +0200

    db

--- pick_fix.sql
+++ pick_fix.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 07, 2020 at 02:53 PM
+-- Generation Time: May 07, 2020 at 03:18 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.5
 
@@ -20,6 +20,22 @@ SET time_zone = "+00:00";
 --
 -- Database: `pick_fix`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `professionals`
+--
+
+CREATE TABLE `professionals` (
+  `PID` int(11) NOT NULL,
+  `FNAME` varchar(20) NOT NULL,
+  `LNAME` varchar(20) NOT NULL,
+  `EMAIL` varchar(25) NOT NULL,
+  `PASSWORD` varchar(255) NOT NULL,
+  `AREA_CODE` int(3) NOT NULL,
+  `PHONE_NUMBER` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -49,6 +65,13 @@ INSERT INTO `users` (`UID`, `FNAME`, `LNAME`, `USERNAME`, `EMAIL`, `PASSWORD`, `
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `professionals`
+--
+ALTER TABLE `professionals`
+  ADD PRIMARY KEY (`PID`),
+  ADD UNIQUE KEY `PID` (`PID`);
 
 --
 -- Indexes for table `users`
