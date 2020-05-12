e2870708f8d485f228ce045316a259b44a9642dc
[Dzenita Djulovic] <[djulovic.dzenita1999@gmail.com]>
2020-05-13 00:54:47 +0200

    made 2 blocks of services in nav dropdown

--- includes/header.php
+++ includes/header.php
@@ -2,24 +2,28 @@
     <div id="inner-header">
         <div id="logo"><a href="../index.php"><h1>Pick & Fix</h1></a></div>
         <nav id="services">
-            <ul><li>
-                    <a href="../findProfessionals.php"><i class="fa fa-angle-right" aria-hidden="true"></i> All Services</a>
-                    <ul>
-                        <li><a href="#">Appliances</a></li>
-                        <li><a href="#">Carpet</a></li>
-                        <li><a href="#">Chimney</a></li>
-                        <li><a href="#">Driveways</a></li>
-                        <li><a href="#">Electrical</a></li>
-                        <li><a href="#">Furniture</a></li>
-                        <li><a href="#">General Repairman</a></li>
-                        <li><a href="#">Glass and Screens</a></li>
-                        <li><a href="#">Lighting</a></li>
-                        <li><a href="#">Painting</a></li>
-                        <li><a href="#">Plumbing</a></li>
-                        <li><a href="#">Windows and Doors</a></li>
-                    </ul>
-                </li>
-            </ul>
+            <div class="dropdown">
+                <a class="dropdown-link" href="../findProfessionals.php"><i class="fa fa-angle-right"
+                                                                            aria-hidden="true"></i> All Services</a>
+                <div class="dropdown-content">
+                    <div>
+                        <a href="#">Appliances</a>
+                        <a href="#">Carpet</a>
+                        <a href="#">Chimney</a>
+                        <a href="#">Driveways</a>
+                        <a href="#">Electrical</a>
+                        <a href="#">Furniture</a>
+                    </div>
+                    <div>
+                        <a href="#">General Repairman</a>
+                        <a href="#">Glass and Screens</a>
+                        <a href="#">Lighting</a>
+                        <a href="#">Painting</a>
+                        <a href="#">Plumbing</a>
+                        <a href="#">Windows and Doors</a>
+                    </div>
+                </div>
+            </div>
         </nav>
         <nav id="login">
             <ul>
