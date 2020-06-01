7df9279d782cc9be2105e234ea8174b4a113a367
Armin Salihovic <armin.salihovic@live.com>
2020-06-01 20:07:25 +0200

    optimized header code

--- includes/header.php
+++ includes/header.php
@@ -26,64 +26,6 @@ while ($row_of_services = oci_fetch_assoc($query_services)){
             </a>
         </span>
 
-        <?php if(empty($_SESSION)): ?>
-        <div id="side-menu" class="side-nav">
-            <a href="#" class="btn-close" onclick="closeMenu()">&times;</a>
-            <a href="#">Home</a>
-            <a href="index.php#how-it-works" onclick="closeMenu()">How It Works?</a>
-            <a href="index.php#story" onclick="closeMenu()">Our Story</a>
-            <a href="index.php#team-overview" onclick="closeMenu()">Team Members</a>
-            <a href="login.php">Find a Professional</a>
-            <a href="login.php">Login</a>
-            <a href="userRegistration.php">Register</a>
-            <a href="professionalsRegistration.php">Join As a Pro</a>
-            <a href="index.php#contact" onclick="closeMenu()">Contact Us</a>
-        </div>
-
-        <div id="logo"><a href="index.php"><h1>Pick & Fix</h1></a></div>
-        <nav id="services">
-
-            <div class="dropdown">
-                <a class="dropdown-link" href="findProfessionals.php"><i class="fa fa-angle-right"
-                                                                         aria-hidden="true"></i> All Services</a>
-                <div class="dropdown-content">
-                    <div>
-                        <a href="#">Appliances</a>
-                        <a href="#">Carpet</a>
-                        <a href="#">Chimney</a>
-                        <a href="#">Driveways</a>
-                        <a href="#">Electrical</a>
-                        <a href="#">Furniture</a>
-                    </div>
-                    <div>
-                        <a href="#">General Repairman</a>
-                        <a href="#">Glass and Screens</a>
-                        <a href="#">Lighting</a>
-                        <a href="#">Painting</a>
-                        <a href="#">Plumbing</a>
-                        <a href="#">Windows and Doors</a>
-                    </div>
-                </div>
-            </div>
-        </nav>
-        <nav id="login">
-            <ul>
-                <li><a href="login.php"><i class="fa fa-sign-in"></i> Log In</a></li>
-                <li><a href="professionalsRegistration.php"><i class="fa fa-star" aria-hidden="true"></i> Join as a Pro</a></li>
-            </ul>
-        </nav>
-    </div>
-    <?php else: ?>
-        <div id="side-menu" class="side-nav">
-            <a href="#" class="btn-close" onclick="closeMenu()">&times;</a>
-            <a href="index.php">Home</a>
-            <a href="findProfessionals.php">Find a Professional</a>
-            <?php if ($row['ROLE'] == 2){ ?>
-                <a href="become-pro.php">Become a Professional</a>
-            <?php } ?>
-            <a href="pro-profile.php">My Profile</a>
-        </div>
-
         <div id="logo"><a href="index.php"><h1>Pick & Fix</h1></a></div>
         <nav id="services">
             <div class="dropdown">
@@ -103,6 +45,29 @@ while ($row_of_services = oci_fetch_assoc($query_services)){
                 </div>
             </div>
         </nav>
+
+        <?php if(empty($_SESSION)): ?>
+        <div id="side-menu" class="side-nav">
+            <a href="#" class="btn-close" onclick="closeMenu()">&times;</a>
+            <a href="#">Home</a>
+            <a href="index.php#how-it-works" onclick="closeMenu()">How It Works?</a>
+            <a href="index.php#story" onclick="closeMenu()">Our Story</a>
+            <a href="index.php#team-overview" onclick="closeMenu()">Team Members</a>
+            <a href="login.php">Find a Professional</a>
+            <a href="login.php">Login</a>
+            <a href="userRegistration.php">Register</a>
+            <a href="professionalsRegistration.php">Join As a Pro</a>
+            <a href="index.php#contact" onclick="closeMenu()">Contact Us</a>
+        </div>
+
+        <nav id="login">
+            <ul>
+                <li><a href="login.php"><i class="fa fa-sign-in"></i> Log In</a></li>
+                <li><a href="professionalsRegistration.php"><i class="fa fa-star" aria-hidden="true"></i> Join as a Pro</a></li>
+            </ul>
+        </nav>
+    </div>
+    <?php else: ?>
         <nav id="login">
             <div class="dropdown">
                 <p class="dropdown-link"><i class="fa fa-user" aria-hidden="true"></i><?php echo ' ' . $_SESSION['fname']. ' ' . $_SESSION['lname'] ?></p>
@@ -117,7 +82,30 @@ while ($row_of_services = oci_fetch_assoc($query_services)){
         </div>
     <?php endif; ?>
 
-
+    <?php if(basename($_SERVER['REQUEST_URI']) == "pro-profile.php" || basename($_SERVER['REQUEST_URI']) == "editProfile.php" || basename($_SERVER['REQUEST_URI']) == "pro-profile-requests.php"): ?>
+        <div id="side-menu" class="side-nav">
+            <a href="#" class="btn-close" onclick="closeMenu()">&times;</a>
+            <a href="index.php">Home</a>
+            <a href="pro-profile.php" onclick="closeMenu()">My Profile</a>
+            <a href="editProfile.php">Edit profile</a>
+            <a href="pro-profile-requests.php" onclick="closeMenu()">My Requests</a>
+            <a href="findProfessionals.php">Find a Professional</a>
+            <?php if ($row['ROLE'] == 2){ ?>
+                <a href="become-pro.php?id=<?= $_SESSION['user_id']?>">Become a Professional</a>
+            <?php } ?>
+            <a href="index.php">Log out</a>
+        </div>
+    <?php else: ?>
+        <div id="side-menu" class="side-nav">
+            <a href="#" class="btn-close" onclick="closeMenu()">&times;</a>
+            <a href="index.php">Home</a>
+            <a href="findProfessionals.php">Find a Professional</a>
+            <?php if ($row['ROLE'] == 2){ ?>
+                <a href="become-pro.php">Become a Professional</a>
+            <?php } ?>
+            <a href="pro-profile.php">My Profile</a>
+        </div>
+    <?php endif; ?>
 
     <script>
         function openMenu(x) {
