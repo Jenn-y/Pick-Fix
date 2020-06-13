6cb9e555adf2475f5956aa52c158e747bebdbb03
[Dzenita Djulovic] <[djulovic.dzenita1999@gmail.com]>
2020-06-14 01:54:23 +0200

    main admin page completed

--- admin/users/users.php
+++ admin/users/users.php
@@ -31,7 +31,6 @@ if (isset($_POST['role'])){
     <?php include('../../includes/head.php') ?>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <link rel="stylesheet" href="../admin.css">
-    <link rel="stylesheet" href="../test.css">
     <link rel="icon" href="../../images/hammer.png">
     <title>Admin | Users</title>
 </head>
